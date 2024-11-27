44f940ae4cea688c11da3be2e37338cf64e52e63
Espinac0 <davidgonncar@gmail.com>
2024-11-27 19:27:09 +0100

    sfad

---
+++ example.py
@@ -0,0 +1,18 @@
+OK_RESULT = 'yes'
+FATAL_RESULT = 'no'
+pepe = 9
+
+
+def check_if_aprive_or_not() -> str:
+
+    result = input('¿Eres del madrid?')
+
+    if isinstance(result, str):
+        if result.lower() == 'yes':
+            return('Aprobado')
+        return 'MEGASUSPENDIDO'
+    
+print(check_if_aprive_or_not())
+
+def foo():
+    pass
